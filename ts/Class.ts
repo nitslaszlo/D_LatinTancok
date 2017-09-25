ebf0a45bb36618234914a490beb8d7939f778dc9
faust.gabor@gmail.hu <faust.gabor@jedlik.helyi>
2017-09-25 13:35:50 +0200

    Class

---
+++ ts/Class.ts
@@ -0,0 +1,11 @@
+export class Táncok {
+   név1: string;
+   név2: string;
+   TáncNeve: string;
+
+   constructor(nev1: string, nev2: string, tancnev: string) {
+      this.név1 = nev1;
+      this.név2 = nev2;
+      this.TáncNeve = tancnev;
+   }
+}
